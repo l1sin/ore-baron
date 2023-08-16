9de5eec062babdf90d48527efec83ae78571d8d1
Andrey Lisin <andrey.lisin11@gmail.com>
2023-08-16 08:19:14 +0500

    Yandex plugin

---
+++ Ore Baron/Assets/Plugins/my.jslib
@@ -0,0 +1,6 @@
+mergeInto(LibraryManager.library, {
+
+  Hello: function () {
+    console.log(player.getName());
+  },
+});
---
+++ Ore Baron/Assets/Scripts/Yandex.cs
@@ -0,0 +1,13 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Yandex : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+    private void Start()
+    {
+        Hello();
+    }
+}
